5e70ac4f39e8aa9ffe77830b406fd5c83f20aaa3
unknown <omerchetin19@gmail.com>
2021-06-11 16:42:04 +0300

    first commit

---
+++ script.py
@@ -0,0 +1,13 @@
+# importing libraries
+from docx import Document
+import requests
+
+
+# createing a .docx file
+doc = Document()
+
+
+# input how many students do you want to get
+students_number = int(input('students number : '))
+
+
