7060075659a279a96437884f8bc0df9ad8f544a8
unknown <omerchetin19@gmail.com>
2021-06-13 14:06:10 +0300

    sending data from form site to the firebase code completed

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -2,13 +2,19 @@ import React , {useEffect, useState} from 'react'
 import {db} from './firebase.jsx'
 import './app.css'
 
-function App() {
+export default function App() {
 
   const [Name, setName] = useState('')
+  const [Gender, setGender] = useState('')
+  const [Age, setAge] = useState('')
+  const [City, setCity] = useState('')
+  const [Email, setEmail] = useState('')
+  const [Phone, setPhone] = useState('')
+
+
 
   const [studentDetails, setstudentDetails] = useState([])
     useEffect(()=>{
-
       const students = []
         db.collection('data').get()
             .then(snapshot => {
@@ -24,25 +30,35 @@ function App() {
                 console.log(studentDetails)
               },[])
               
-              const sendData = ()=>{
-                db.collection('data').add({
-        'name': 'omar chatin',
-        'age':'15'
-      })
-    }
-    
-    
-    
+      const sendData = ()=>{
+          db.collection('data').add({
+          'name': Name,
+          'age':Age,
+          'gender':Gender,
+          'State':City,
+          'email':Email,
+          'phone':Phone
+        })
+      }
+
+
+
+
+        
     return (
-      <div>
-        <div className="container">  
-          <form id="contact" action="" method="post">
+      <div className="container">  
+          <form id="contact">
             <h3>تقنيات صناعة الاسنان</h3>
             <h4>جامعة الكتاب</h4>
             
             
             <fieldset>
-              <input placeholder="الاسم" type="text" tabindex="1" required autofocus/>
+              <input placeholder="الاسم" onChange={(e)=>{setName(e.target.value)}} type="text" tabindex="1" required autofocus/>
+            </fieldset>
+
+
+            <fieldset>
+              <input placeholder="العمر" onChange={(e)=>{setAge(e.target.value)}} type="text" tabindex="1" required autofocus/>
             </fieldset>
 
             
@@ -53,11 +69,11 @@ function App() {
 
                 <label htmlFor="">
                   ذكر
-                  <input value='male' required name='gender' type="radio"/>  
+                  <input value='male' onChange={(e)=>{setGender(e.target.value)}} required name='gender' type="radio"/>  
                 </label>
                 <label htmlFor="">
                   انثى
-                  <input value='female' required name='gender' type="radio"/>
+                  <input value='female' onChange={(e)=>{setGender(e.target.value)}} required name='gender' type="radio"/>
                 </label>
               </div>
 
@@ -66,25 +82,23 @@ function App() {
 
 
             <fieldset>
-              <input placeholder="الايميل الجامعي" type="text" tabindex="2" required/>
+              <input placeholder="الايميل الجامعي" onChange={(e)=>{setEmail(e.target.value)}} type="text" tabindex="2" required/>
+            </fieldset>
+
+
+            <fieldset>
+              <input placeholder="رقم الهاتف" onChange={(e)=>{setPhone(e.target.value)}} type="tel" tabindex="3" required/>
             </fieldset>
             <fieldset>
-              <input placeholder="رقم الهاتف" type="tel" tabindex="3" required/>
-            </fieldset>
-            <fieldset>
-              <input placeholder="من اي محافضة" type="text" tabindex="4" required/>
+              <input placeholder="من اي محافضة" onChange={(e)=>{setCity(e.target.value)}} type="text" tabindex="4" required/>
             </fieldset>
 
             <fieldset>
-              <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">الارسال</button>
+              <button name="submit" type="submit" id="contact-submit" onClick={sendData}>الارسال</button>
             </fieldset>
             <p className="copyright">by omar chatin</p>
             <p className="copyright">source code on <a href="" target="_blank" title="Colorlib">my github</a></p>
           </form>
-      </div>  
-    </div>
+      </div>
   )
 }
-
-export default App
-
