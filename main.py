810ff4440131997bdaa8e14070f56bb8630a45c6
unknown <omerchetin19@gmail.com>
2021-06-13 21:01:02 +0300

    fetching data with python code from firebase/firestore

---
+++ main.py
@@ -0,0 +1,115 @@
+# importing libraries/////////////////////                
+                                    #/////
+from docx import Document           #/////                    
+import requests                     #/////        
+from colorama import init           #/////                    
+from termcolor import colored       #/////                        
+init()                              #/////
+import time                         #/////    
+import os                           #/////    
+import firebase_admin               #/////                
+from firebase_admin import credentials#///                         
+from firebase_admin import firestore#/////                                
+from docx.shared import Inches, Cm  #/////                            
+                                    #/////
+#/////////////////////////////////////////
+
+#createing the table and word file////////
+#/////////////////////////////////////////
+                                    #/////
+word = Document()                   #///// 
+table = word.add_table(rows=1,cols=5)#////
+table.style = 'Table Grid'          #/////
+th_cells = table.rows[0].cells      #/////
+th_cells[0].text = 'name'           #/////
+th_cells[1].text = 'gender'         #/////
+th_cells[2].text = 'email'          #/////
+th_cells[3].text = 'phone'          #/////
+th_cells[4].text = 'State'          #/////
+                                    #/////
+# page layout/////////////////////////////
+#///////////////////////////////////////// 
+                                    #/////
+sections = word.sections            #/////
+for section in sections:            #/////
+    section.top_margin = Cm(0.5)    #/////
+    section.bottom_margin = Cm(0.5) #/////
+    section.left_margin = Cm(1)     #/////
+    section.right_margin = Cm(1)    #/////
+                                    #/////
+#/////////////////////////////////////////
+#/////////////////////////////////////////
+
+
+os.system('cls||clear')
+print(colored('\n--------------------\ automate MS word with python /--------------------', 'red'))
+print(colored('----------------------------\ by omar chatin /----------------------------\n\n', 'red'))
+
+
+
+rqst = input(colored('type "rdm" to get random data from api or type "db" to get real data from database : ','green'))
+
+
+
+# getting data from firebase
+if rqst=='db':
+    print(colored('fetching data from database...\n', 'red'))
+    time.sleep(4)
+    cred = credentials.Certificate("./serviceAccountKey.json")
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    docs = db.collection(u'data').stream()
+
+    print(docs)
+    for prs in docs:
+        person = prs.to_dict()
+        a = table.add_row().cells
+        a[0].text = person['name']
+        a[1].text = person['gender']
+        a[2].text = person['email']
+        a[3].text = person['phone']
+        a[4].text = person['State']
+
+
+
+
+
+# getting data from random user api
+if rqst=='rdm':
+
+    num = int(input(colored('how many people do you want to get ? : ','green')))
+    print(colored('\nfetching data from randomuser API...', 'red'))
+    time.sleep(4)
+    data = requests.get(f'https://randomuser.me/api/?results={num}')
+    data = data.json()['results']
+    for i in data:
+        person={
+            "name" : i["name"]["first"] + i["name"]["last"],
+            "gender" : i["gender"],
+            "email" : i["email"],
+            "phone" : i["phone"],
+            "location" : i["location"]["country"] + ' / ' +  i["location"]["city"],
+        }
+
+        a = table.add_row().cells
+        a[0].text = person['name']
+        a[1].text = person['gender']
+        a[2].text = person['email']
+        a[3].text = person['phone']
+        a[4].text = person['location']
+
+
+
+
+
+word.save('demo2.docx')
+time.sleep(3)
+os.system('cls')
+
+print(colored('\n---- finished processing ----', 'red'))
+time.sleep(2)
+
+print(colored('\n now openning the word file automatically...', 'red'))
+time.sleep(2)
+
+os.system('start demo2.docx')
--- script.py
+++ script.py
@@ -6,6 +6,23 @@ from termcolor import colored
 init()
 import time
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+docs = db.collection(u'data').stream()
+
+
+
+
+
 
 
 
@@ -13,26 +30,49 @@ import os
 doc = Document()
 
 os.system('cls||clear')
-print(colored('\n---------\ automate MS word with python /------------------', 'red'))
-print(colored('-----------------\ by omar chatin /------------------\n', 'red'))
+print(colored('\n--------------------\ automate MS word with python /--------------------', 'red'))
+print(colored('----------------------------\ by omar chatin /----------------------------\n', 'red'))
+
+count = 0
+
+reqst = input('type "r" to get random data from api or type "s" to get real data : ')
+
 
 
 
 # input how many students do you want to get
-try:
-    students_number = int(input(colored('how many people do you want to get ? : ','green')))
-    print('\n')
-except:
-    print(colored('error occured \n please enter valid number between 0 and 5000\n\n','white', 'on_red'))
-    time.sleep(3)
 
 
 
 
 
 
-# creating the table
-table = doc.add_table(rows=students_number+1,cols=5)
+
+
+if reqst=='s':
+    for dc in docs:
+        students_number = 0
+
+    table = doc.add_table(rows=students_number+1,cols=5)
+    for doc in docs:
+        person = doc.to_dict()
+        count +=1
+        dt_table = table.rows[count].cells
+        dt_table[0].text = person["name"]
+        dt_table[1].text = person["gender"]
+        dt_table[2].text = person["email"]
+        dt_table[3].text = person["phone"]
+        dt_table[4].text = person["State"]
+        print(person["name"])
+        print(count)
+
+else:
+    print(colored('error occured \n write "r" to get random data or type "s" to get real data \n\n','white', 'on_red'))
+
+
+
+
+
 
 
 
@@ -47,35 +87,44 @@ th_table[2].text = 'email'
 th_table[3].text = 'phone'
 th_table[4].text = 'location'
 
-
+print(colored('collecting data from api ...','green'))
 
 # requesting data
 data = requests.get(f'https://randomuser.me/api/?results={students_number}')
 
-count = 0
 
 data = data.json()['results']
-for i in data:
-    count +=1
-    person={
-        "name" : i["name"]["first"] + i["name"]["last"],
-        "gender" : i["gender"],
-        "email" : i["email"],
-        "phone" : i["phone"],
-        "location" : i["location"]["country"] + ' / ' +  i["location"]["city"],
-    }
-
-    print(colored(f'prson {count} data: ', 'yellow'))
-    print(person, '\n')
 
 
 
-    dt_table = table.rows[count].cells
-    dt_table[0].text = person["name"]
-    dt_table[1].text = person["gender"]
-    dt_table[2].text = person["email"]
-    dt_table[3].text = person["phone"]
-    dt_table[4].text = person["location"]
+if reqst=='r':
+    table = doc.add_table(rows=students_number+1,cols=5)
+
+    students_number = int(input(colored('how many people do you want to get ? : ','green')))
+    # creating the table
+    table = doc.add_table(rows=students_number+1,cols=5)
+
+    for i in data:
+        count +=1
+        person={
+            "name" : i["name"]["first"] + i["name"]["last"],
+            "gender" : i["gender"],
+            "email" : i["email"],
+            "phone" : i["phone"],
+            "location" : i["location"]["country"] + ' / ' +  i["location"]["city"],
+        }
+
+        print(colored(f'prson {count} data: ', 'yellow'))
+        print(person, '\n')
+
+
+
+        dt_table = table.rows[count].cells
+        dt_table[0].text = person["name"]
+        dt_table[1].text = person["gender"]
+        dt_table[2].text = person["email"]
+        dt_table[3].text = person["phone"]
+        dt_table[4].text = person["location"]
 
 
 
@@ -99,7 +148,6 @@ time.sleep(2)
 
 print(colored('\n now openning the word file ...', 'red'))
 time.sleep(2)
-
 
 # openning the .docx file
 os.system('start demo.docx')
