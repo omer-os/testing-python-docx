fff457cac47b227402d8069635402fab4cdc3495
unknown <omerchetin19@gmail.com>
2021-06-13 12:25:42 +0300

    adding the form

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,6 +1,6 @@
 import React , {useEffect, useState} from 'react'
 import {db} from './firebase.jsx'
-import { FormHelperText, FormControl, InputLabel , Input } from '@material-ui/core';
+import './app.css'
 
 function App() {
 
@@ -13,35 +13,75 @@ function App() {
         db.collection('data').get()
             .then(snapshot => {
                 snapshot.docs.forEach(student => {
-                    let currentID = student.id
-                    let appObj = { ...student.data(), ['id']: currentID }
-                    students.push(appObj)
-
+                  let currentID = student.id
+                  let appObj = { ...student.data(), ['id']: currentID }
+                  students.push(appObj)
+                  
                     students.push(student.data())
-            })
-            setstudentDetails(students)
-        })
-        console.log(studentDetails)
-    },[])
-
-    const sendData = ()=>{
-      db.collection('data').add({
+                  })
+                  setstudentDetails(students)
+                })
+                console.log(studentDetails)
+              },[])
+              
+              const sendData = ()=>{
+                db.collection('data').add({
         'name': 'omar chatin',
         'age':'15'
       })
     }
+    
+    
+    
+    return (
+      <div>
+        <div className="container">  
+          <form id="contact" action="" method="post">
+            <h3>تقنيات صناعة الاسنان</h3>
+            <h4>جامعة الكتاب</h4>
+            
+            
+            <fieldset>
+              <input placeholder="الاسم" type="text" tabindex="1" required autofocus/>
+            </fieldset>
+
+            
+            <fieldset className='radios'>
+
+              <div className='inside_radios'>
+                <p>الجنس</p>
+
+                <label htmlFor="">
+                  ذكر
+                  <input value='male' required name='gender' type="radio"/>  
+                </label>
+                <label htmlFor="">
+                  انثى
+                  <input value='female' required name='gender' type="radio"/>
+                </label>
+              </div>
+
+            </fieldset>
 
 
 
-  return (
-    <div>
-      {/* <TextField value={Name} onChange={(e)=>{setName(e.target.value)}} id="standard-basic" label="Your Name" />
-      <Button onClick={sendData}>Submit</Button> */}
-      <FormControl>
-        <InputLabel htmlFor="my-input">Email address</InputLabel>
-        <Input id="my-input" aria-describedby="my-helper-text" />
-        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-    </FormControl>
+            <fieldset>
+              <input placeholder="الايميل الجامعي" type="text" tabindex="2" required/>
+            </fieldset>
+            <fieldset>
+              <input placeholder="رقم الهاتف" type="tel" tabindex="3" required/>
+            </fieldset>
+            <fieldset>
+              <input placeholder="من اي محافضة" type="text" tabindex="4" required/>
+            </fieldset>
+
+            <fieldset>
+              <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">الارسال</button>
+            </fieldset>
+            <p className="copyright">by omar chatin</p>
+            <p className="copyright">source code on <a href="" target="_blank" title="Colorlib">my github</a></p>
+          </form>
+      </div>  
     </div>
   )
 }
